71c953abc6b91cc6f44d9333731abe388599e2dd
gusttavogomes <30482020+gusttavogomes@users.noreply.github.com>
2020-06-17 17:19:17 -0300

    Fotos

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import logo from './logo.svg';
-import './App.css';
+import './css/App.css';
 
 function App() {
   return (
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Vadim Blyat.
         </p>
         <a
           className="App-link"
