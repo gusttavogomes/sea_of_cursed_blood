35e4e98426eb3d9db822f3631d77dfc69c5f7a71
gusttavogomes <30482020+gusttavogomes@users.noreply.github.com>
2020-06-23 16:40:42 -0300

    Update Rule.js
    Classes

--- src/components/Rule.js
+++ src/components/Rule.js
@@ -14,7 +14,7 @@ export default function Rule() {
                         </p>                    
                     <h2>Classes Liberadas</h2>
                         <p>
-                        Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum 
+                        As 12 classes das Regras Básicas e do Livro do Jogador estão liberadas, assim como a de <i>Eberron - Riding from the Last War</i>, totalizando: <i>Artificer, Barbarian, Bard, Cleric, Druid, Fighter, Monk, Paladin, Ranger, Rogue, Sorcerer, Warlock</i> e <i>Wizard </i>. <i>Blood Hunter</i> pode ser liberado de acordo com a vontade do Mestre. As subclasses estão todas liberadas dos principais livros oficiais. Eventuais suplementos oficiais podem ser incluidos, mas depende de cada Mestre aprovar ou não.
                         </p>                           
                 </Jumbotron>
             </div>
