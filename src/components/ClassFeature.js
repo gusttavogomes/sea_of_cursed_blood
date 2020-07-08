6f3845904d15c05dce23874d6dd3dc475552ad65
gusttavogomes <30482020+gusttavogomes@users.noreply.github.com>
2020-07-08 10:22:33 -0300

    Arrumando a fonte do pop-up

--- src/components/ClassFeature.js
+++ src/components/ClassFeature.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import '../css/ClassFeature.css';
 import { Modal, Button } from 'react-bootstrap';
 
 export default function MyVerticallyCenteredModal(props) {
@@ -16,33 +17,33 @@ export default function MyVerticallyCenteredModal(props) {
         </Modal.Header>
         <Modal.Body>
           <h4>Explorador nato</h4>
-          <p>
+          <p id='descricao_habilidade'>
             O capitão pode dar ou usar um bônus de +5 em testes de
             investigação, sobrevivência ou percepção relacionados à exploração de locais. 1 vez
             por descanso longo.
           </p>
           <h4>Mobilidade no navio:</h4>
-          <p>
+          <p id='descricao_habilidade'>
             O capitão conhece o navio como a palma de sua mão, o
             capitão ganha metade de sua movimentação padrão como bônus quando esta dentro
             de seu navio.
           </p>
           <h4>Esconder-se</h4>
-          <p>
+          <p id='descricao_habilidade'>
             O capitão do navio pode engajar-se nesta atividade apenas se as
             condições climáticas restringem visibilidade, como uma névoa densa. O navio faz um
             teste de Destreza com um bônus igual ao valor de qualidade da tripulação para
             determinar se ele pode se esconder.
           </p>
           <h4>Ataquem homens</h4>
-          <p>
+          <p id='descricao_habilidade'>
             O capitão com sua ação bônus pode fazer com os tripulantes há
             15 pés dele possam gastar suas reações para realizar uma ataque em uma criatura que
-            o capitão desejar. A habilidade recarrega com um roll de 5 ou 6 no D6.
+            o capitão desejar. A habilidade recarrega com um <i>roll</i> de 5 ou 6 no D6.
           </p>
         </Modal.Body>
         <Modal.Footer>
-          <Button onClick={props.onHide}>Close</Button>
+          <Button onClick={props.onHide}>Fechar</Button>
         </Modal.Footer>
       </Modal>
     );
