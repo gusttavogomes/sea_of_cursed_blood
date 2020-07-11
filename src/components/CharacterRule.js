09f6da8dc223d0efce2756cfd3d2ddee7e2da19f
gusttavogomes <30482020+gusttavogomes@users.noreply.github.com>
2020-07-10 21:39:11 -0300

    Arrumando o NavBar
    Ele tava redirecionando apenas pra URL e não para o local em si do elemento buscado

--- src/components/CharacterRule.js
+++ src/components/CharacterRule.js
@@ -7,15 +7,17 @@ export default function CharacterRule() {
         <div id='rule'> 
             <div className="container">
                 <Jumbotron className="jbt-character-rule">
-                    <h1>Criação de personagens</h1>
-                    <h2>Raças Liberadas</h2>
-                        <p>
-                        Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum 
-                        </p>                    
-                    <h2>Classes Liberadas</h2>
-                        <p>
-                        As 12 classes das Regras Básicas e do Livro do Jogador estão liberadas, assim como a de <i>Eberron - Rising from the Last War</i>, totalizando: <i>Artificer, Barbarian, Bard, Cleric, Druid, Fighter, Monk, Paladin, Ranger, Rogue, Sorcerer, Warlock</i> e <i>Wizard </i>. <i>Blood Hunter</i> pode ser liberado de acordo com a vontade do Mestre. As subclasses estão todas liberadas dos principais livros oficiais. Eventuais suplementos oficiais podem ser incluidos, mas depende de cada Mestre aprovar ou não.
-                        </p>                           
+                    <div id="criacao-de-personagem">
+                        <h1>Criação de personagens</h1>
+                        <h2>Raças Liberadas</h2>
+                            <p>
+                            Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum 
+                            </p>                    
+                        <h2>Classes Liberadas</h2>
+                            <p>
+                            As 12 classes das Regras Básicas e do Livro do Jogador estão liberadas, assim como a de <i>Eberron - Rising from the Last War</i>, totalizando: <i>Artificer, Barbarian, Bard, Cleric, Druid, Fighter, Monk, Paladin, Ranger, Rogue, Sorcerer, Warlock</i> e <i>Wizard </i>. As subclasses estão todas liberadas dos principais livros oficiais. Eventuais suplementos oficiais podem ser incluidos, mas depende de cada Mestre aprovar ou não. Apesar de não oficial, <i>Bloodhunter</i> pode ser liberado de acordo com a vontade do Mestre, assim como qualquer outro <i>homebrow</i> criado por terceiros, já que o objetivo dessa aventura é todos se divertirem.
+                            </p> 
+                    </div>                          
                 </Jumbotron>
             </div>            
         </div>
--- src/components/GameplayRule.js
+++ src/components/GameplayRule.js
@@ -17,87 +17,88 @@ export default function CharacterRule() {
         <div id='rule'> 
             <div className="container">
                 <Jumbotron className="jbt-gameplay-rule">                                     
-                    <h1>Gerenciamento do Navio</h1>
-                        <p>
-                        Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum 
-                        </p>   
-                    <h1>Mecânicas dos Oficiais</h1>
-                        <p> Para comandar o navio na aventura, é necessário uma hieraquia de comando e funções e responsabilidades designadas a certas pessoas sendo os principais dessa estrutura os Oficiais. São ao todo seis oficiais: <b> Capitão, Imediato, Contramestre, Quartel-Mestre, Cirurgião e Cozinheiro</b>. Além da tribulação normal que varia de acordo com o navio é necessário que se tenha tais oficiais para que a campanha funcione melhor. Claro que se não tiver jogadores suficientes, tais cargos podem ser substituídos por NPC's. Cada oficial tem habilidades habilidades para o combate ou fora dele, alguns com mais que os outros, mas nada impede do Mestre alterar tais habilidades ou adicionar novas. A seguir são descrito as funções básicas de cada oficial e nos botões expandem as informações mostrando as habilidades de cada oficial e as informações sobre cada uma dessa mecânicas.</p>
-                    
-                    <h2>Capitão</h2>
-                        <p>O capitão emite ordens. Os melhores capitães possuem altos valores de Inteligência e Carisma, bem como proficiência com veículos aquáticos e nas perícias Persuasão e Intimidação.</p>                        
-                    <Link 
-                        to={'capitao'}
-                        spy={true}
-                        smooth={true}
-                        offset={0}
-                        duration= {2000}
-                    >
-                        <Button variant="outline-light">Ver habilidades de Capitão</Button> 
-                    </Link>
-
-                    <h2>Imediato</h2>
-                        <p>Este especialista mantém a moral da tripulação alta ao prover supervisão de perto, encorajamento e disciplina. Um imediato se beneficia de um alto valor de Carisma, bem como proficiência nas perícias Intimidação e Persuasão.</p> 
-                    <Link 
-                        to={'imediato'}
-                        spy={true}
-                        smooth={true}
-                        offset={0}
-                        duration= {2000}
-                    >
-                        <Button variant="outline-light">Ver habilidades de Imediato</Button> 
-                    </Link>
-
-                    <h2>Contramestre</h2>
-                        <p>O Contramestre (também conhecido como pequeno oficial) provê conselhos técnicos para o capitão e os tripulantes e lidera os esforços de reparo e manutenção. Um bom contramestre possui um grande valor de atributo em Força, bem como proficiência com ferramentas de carpinteiro e na perícia Atletismo.</p>  
-                    <Link 
-                        to={'contramestre'}
-                        spy={true}
-                        smooth={true}
-                        offset={0}
-                        duration= {2000}
-                    >                    
-                        <Button variant="outline-light">Ver habilidades de Contramestre</Button> 
-                    </Link>
-
-                    <h2>Quartel-Mestre</h2>
-                        <p>O Quartel-Mestre traça o curso do navio, confiando em conhecimentos de mapas náuticos e no estudo do clima e das condições do mar. Um quartel-mestre confiável tende a possuir um valor alto de Sabedoria, bem como proficiência nas ferramentas de navegador e na perícia Natureza.</p>  
-                    <Link  
-                        to={'quartel-mestre'}
-                        spy={true}
-                        smooth={true}
-                        offset={0}
-                        duration= {2000}
-                    >
-                        <Button variant="outline-light">Ver habilidades de Quartel-Mestre</Button> 
-                    </Link> 
-                   
-                    <h2>Cirurgião</h2>
-                        <p>O cirurgião do navio pode cuidar de machucados, impede doenças de se espalhar por todo o navio e supervisiona o saneamento. Um cirurgião capaz se beneficia de altos valores de Inteligência ou sabedoria, bem como proficiência em kits de herbalismo e na perícia Medicina.</p>  
-                    
-                    <Link 
-                        to={'cirurgiao'}
-                        spy={true}
-                        smooth={true}
-                        offset={0}
-                        duration= {2000}
-                    >
-                        <Button variant="outline-light">Ver habilidades de Cirurgião </Button>  
-                    </Link>
+                    <div id="gerenciamento-do-navio">
+                        <h1>Gerenciamento do Navio</h1>
+                            <p>
+                            Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum 
+                            </p>  
+                    </div> 
+                    <div id="mecanicas-dos-oficiais">
+                        <h1>Mecânicas dos Oficiais</h1>
+                            <p> Para comandar o navio na aventura, é necessário uma hieraquia de comando e funções e responsabilidades designadas a certas pessoas sendo os principais dessa estrutura os Oficiais. São ao todo seis oficiais: <b> Capitão, Imediato, Contramestre, Quartel-Mestre, Cirurgião e Cozinheiro</b>. Além da tribulação normal que varia de acordo com o navio é necessário que se tenha tais oficiais para que a campanha funcione melhor. Claro que se não tiver jogadores suficientes, tais cargos podem ser substituídos por NPC's. Cada oficial tem habilidades habilidades para o combate ou fora dele, alguns com mais que os outros, mas nada impede do Mestre alterar tais habilidades ou adicionar novas. A seguir são descrito as funções básicas de cada oficial e nos botões expandem as informações mostrando as habilidades de cada oficial e as informações sobre cada uma dessa mecânicas.</p>
                         
+                        <h2>Capitão</h2>
+                            <p>O capitão emite ordens. Os melhores capitães possuem altos valores de Inteligência e Carisma, bem como proficiência com veículos aquáticos e nas perícias Persuasão e Intimidação.</p>                        
+                        <Link 
+                            to={'capitao'}
+                            spy={true}
+                            smooth={true}
+                            offset={0}
+                            duration= {2000}
+                        >
+                            <Button variant="outline-light">Ver habilidades de Capitão</Button> 
+                        </Link>
+
+                        <h2>Imediato</h2>
+                            <p>Este especialista mantém a moral da tripulação alta ao prover supervisão de perto, encorajamento e disciplina. Um imediato se beneficia de um alto valor de Carisma, bem como proficiência nas perícias Intimidação e Persuasão.</p> 
+                        <Link 
+                            to={'imediato'}
+                            spy={true}
+                            smooth={true}
+                            offset={0}
+                            duration= {2000}
+                        >
+                            <Button variant="outline-light">Ver habilidades de Imediato</Button> 
+                        </Link>
+
+                        <h2>Contramestre</h2>
+                            <p>O Contramestre (também conhecido como pequeno oficial) provê conselhos técnicos para o capitão e os tripulantes e lidera os esforços de reparo e manutenção. Um bom contramestre possui um grande valor de atributo em Força, bem como proficiência com ferramentas de carpinteiro e na perícia Atletismo.</p>  
+                        <Link 
+                            to={'contramestre'}
+                            spy={true}
+                            smooth={true}
+                            offset={0}
+                            duration= {2000}
+                        >                    
+                            <Button variant="outline-light">Ver habilidades de Contramestre</Button> 
+                        </Link>
+
+                        <h2>Quartel-Mestre</h2>
+                            <p>O Quartel-Mestre traça o curso do navio, confiando em conhecimentos de mapas náuticos e no estudo do clima e das condições do mar. Um quartel-mestre confiável tende a possuir um valor alto de Sabedoria, bem como proficiência nas ferramentas de navegador e na perícia Natureza.</p>  
+                        <Link  
+                            to={'quartel-mestre'}
+                            spy={true}
+                            smooth={true}
+                            offset={0}
+                            duration= {2000}
+                        >
+                            <Button variant="outline-light">Ver habilidades de Quartel-Mestre</Button> 
+                        </Link> 
                     
-                    
-                    <h2>Cozinheiro</h2>
-                        <p>O cozinheiro de um navio trabalha com os limitados ingredientes a bordo de um navio para fazer alimentos. Um cozinheiro habilidoso mantém a moral da tripulação em boa forma, enquanto que um cozinheiro ruim diminui o desempenho de toda a tripulação. Um cozinheiro talentoso tem valores de Sabedoria e destreza altos, bem como proficiências em suprimentos de cervejeiro e utensílios de cozinha.</p>  
-                    <Link 
-                        to={'cozinheiro'}
-                        spy={true}
-                        smooth={true}
-                        offset={0}
-                        duration= {2000}
-                    >
-                        <Button variant="outline-light">Ver habilidades de Cozinheiro</Button> 
-                    </Link>
+                        <h2>Cirurgião</h2>
+                            <p>O cirurgião do navio pode cuidar de machucados, impede doenças de se espalhar por todo o navio e supervisiona o saneamento. Um cirurgião capaz se beneficia de altos valores de Inteligência ou sabedoria, bem como proficiência em kits de herbalismo e na perícia Medicina.</p>                          
+                        <Link 
+                            to={'cirurgiao'}
+                            spy={true}
+                            smooth={true}
+                            offset={0}
+                            duration= {2000}
+                        >
+                            <Button variant="outline-light">Ver habilidades de Cirurgião </Button>  
+                        </Link>
+                        
+                        <h2>Cozinheiro</h2>
+                            <p>O cozinheiro de um navio trabalha com os limitados ingredientes a bordo de um navio para fazer alimentos. Um cozinheiro habilidoso mantém a moral da tripulação em boa forma, enquanto que um cozinheiro ruim diminui o desempenho de toda a tripulação. Um cozinheiro talentoso tem valores de Sabedoria e destreza altos, bem como proficiências em suprimentos de cervejeiro e utensílios de cozinha.</p>  
+                        <Link 
+                            to={'cozinheiro'}
+                            spy={true}
+                            smooth={true}
+                            offset={0}
+                            duration= {2000}
+                        >
+                            <Button variant="outline-light">Ver habilidades de Cozinheiro</Button> 
+                        </Link>
+                    </div>
 
                 </Jumbotron>
                 <Jumbotron className="jbt-gameplay-rule">                        
