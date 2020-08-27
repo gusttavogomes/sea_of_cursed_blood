a60c76e1ec41005b43837120b1d5a02271268afb
gusttavogomes <30482020+gusttavogomes@users.noreply.github.com>
2020-08-27 16:18:42 -0300

    Update Navegation.js

--- src/components/Navegation.js
+++ src/components/Navegation.js
@@ -11,37 +11,35 @@ export default function Navegation() {
     return(        
         <div id='navegation'> 
             <Navbar collapseOnSelect expand="lg" className="color-nav" variant="dark" >
-            <Navbar.Brand href="/">
-            <img
-                alt="Sea of Cursed Blood Logo"
-                src={logo}
-                width="42"
-                height="60"
-                className="d-inline-block align-top"
-            />{' '}
-                {/*Sea of Cursed Blood*/}
-                <h4 id="logo-name">Sea of Cursed Blood</h4>
-            </Navbar.Brand>
-            <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-            <Navbar.Collapse id="responsive-navbar-nav">
-                <Nav className="mr-auto">
-                <Nav.Link href="/history" >História</Nav.Link>
-                <NavDropdown title="Regras" id="collasible-nav-dropdown-cenario">
-                    <NavDropdown.Item href="/rules">Criação de PC's</NavDropdown.Item>
-                    <NavDropdown.Divider />
-                    <NavDropdown.Item href="/rules#gerenciamento-do-navio">Gerenciamento do Navio</NavDropdown.Item>
-                    <NavDropdown.Item href="/rules#mecanicas-dos-oficiais">Mecânicas dos oficiais</NavDropdown.Item>
-                </NavDropdown>
-                
-                <Nav.Link href="/player-character">Player Character (PC's)</Nav.Link>
-                
-                </Nav>
-                <Nav>
-                <Nav.Link href="/art">Artistas/Artists</Nav.Link>                
-                </Nav>
-            </Navbar.Collapse>
-            </Navbar>
-            
+				<Navbar.Brand href="/">
+					<img
+						alt="Sea of Cursed Blood Logo"
+						src={logo}
+						width="42"
+						height="60"
+						className="d-inline-block align-top"
+					/>{' '}
+						{/*Sea of Cursed Blood*/}
+						<h4 id="logo-name">Sea of Cursed Blood</h4>
+				</Navbar.Brand>
+				<Navbar.Toggle aria-controls="responsive-navbar-nav" />
+				<Navbar.Collapse id="responsive-navbar-nav">
+					<Nav className="mr-auto">
+						<Nav.Link href="/history" >História</Nav.Link>
+						<NavDropdown title="Regras" id="collasible-nav-dropdown-cenario">
+							<NavDropdown.Item href="/rules">Criação de PC's</NavDropdown.Item>
+							<NavDropdown.Divider />
+							<NavDropdown.Item href="/rules#gerenciamento-do-navio">Gerenciamento do Navio</NavDropdown.Item>
+							<NavDropdown.Item href="/rules#mecanicas-dos-oficiais">Mecânicas dos oficiais</NavDropdown.Item>
+						</NavDropdown>
+						
+						<Nav.Link href="/player-character">Player Character (PC's)</Nav.Link>						
+					</Nav>
+					<Nav>
+						<Nav.Link href="/art">Artistas/Artists</Nav.Link>                
+					</Nav>
+				</Navbar.Collapse>
+            </Navbar>            
         </div>
         
     );
